8c37e37c5c2f835f8850c200bf730813ce13a928
jotham333 <jothamjosiah10@gmail.com>
2023-08-10 04:44:53 +0100

    ALX

--- models/base_model.py
+++ models/base_model.py
@@ -4,21 +4,23 @@ from datetime import datetime
 class BaseModel:
     """A base class for all models in our hbnb clone."""
     
+    DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+    
     def __init__(self, *args, **kwargs):
         """Initialize a new model."""
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                            setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-        elif key != "__class__":
-            setattr(self, key, value)
-            
         current_time = datetime.now()
         self.id = str(uuid.uuid4())
-        self.created_at = current_time
-        self.updated_at = current_time
+        self.created_at = current_time.isoformat()
+        self.updated_at = current_time.isoformat()
+        if len(kwargs) != 0:
+            # Update instance variables with key-value pairs from kwargs
+            self.__dict__.update({
+                key: datetime.strptime(value, self.DATE_FORMAT)
+                if key in ["created_at", "updated_at"] else value
+                for key, value in kwargs.items() if key != "__class__"
+            })
         
+        self.save()
     
     
     def __str__(self):
@@ -36,4 +38,3 @@ class BaseModel:
         result["created_at"] = self.created_at.isoformat()
         result["updated_at"] = self.updated_at.isoformat()
         return result
-
