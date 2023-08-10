37444d8797e4673be9f3e9916c834a87c793f72c
aabine <kendoaustine@yahoo.com>
2023-08-10 06:23:32 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 import uuid
+from models import storage
 from datetime import datetime
 
 class BaseModel:
@@ -19,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = current_time
             self.updated_at = current_time
+            storage.new(self)
         
     
     
@@ -29,6 +31,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed."""
         self.updated_at = datetime.now()
+        storage.save()
     
     def to_dict(self):
         """Return dictionary representation of BaseModel."""
