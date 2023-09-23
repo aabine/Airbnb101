f67ed92af1d6f3b8479e0c7632127c534b3a87a7
aabine <kendoaustine@yahoo.com>
2023-09-23 01:09:14 +0100

    FIXES

--- new/models/state.py
+++ new/models/state.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
-from models.city import City
 
 
 class State(BaseModel, Base):
@@ -15,6 +14,7 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         from models import storage
+        from models.city import City
         cities_list = []
         for city in storage.all(City).values():
             if city.state_id == self.id:
--- new/setup_mysql_dev.sql
+++ new/setup_mysql_dev.sql
@@ -7,5 +7,8 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
