953119fdbbbfcfbf0411c37ec77eacfbaee8e9f8
aabine <kendoaustine@yahoo.com>
2023-09-23 02:38:08 +0100

    done

--- new/models/amenity.py
+++ new/models/amenity.py
@@ -5,14 +5,8 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
-
-<<<<<<< HEAD
-
 class Amenity(BaseModel, Base):
     """Amenity class"""
-=======
-class Amenity(BaseModel, Base):
->>>>>>> 2b52cc195d188878b4e5aa010ea371ccf8f195c7
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place', secondary=place_amenity)
